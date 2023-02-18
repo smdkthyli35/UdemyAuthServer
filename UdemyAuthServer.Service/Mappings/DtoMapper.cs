a5373cf8de5313a983a12828693b3783f861fa97
Samed Kütahyalı <smdkthyli@gmail.com>
2023-02-18 14:41:23 +0300

    Automapper has been added.

---
+++ UdemyAuthServer.Service/Mappings/DtoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Models;
+
+namespace UdemyAuthServer.Service.Mappings
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
