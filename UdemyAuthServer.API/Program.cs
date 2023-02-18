3124158846459188ead83eab597fa0f4d730df8b
Samed Kütahyalı <smdkthyli@gmail.com>
2023-02-18 16:17:38 +0300

    CustomTokenOption has been added with options pattern.

--- UdemyAuthServer.API/Program.cs
+++ UdemyAuthServer.API/Program.cs
@@ -1,9 +1,15 @@
+using SharedLibrary.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
