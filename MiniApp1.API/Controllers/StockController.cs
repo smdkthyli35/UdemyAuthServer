fe683f8b2a0901252ba1a382496dfffb176c4f7f
Samed Kütahyalı <smdkthyli@gmail.com>
2023-03-12 18:17:15 +0300

    Mini api's controller has been added.

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+            return Ok($"Username: {userName} - UserId: {userId}");
+        }
+    }
+}
